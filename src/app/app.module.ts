a9c3946aa453503d81d59bd826d44872e3832406
santiago <daniel.lara07c@gmail.com>
2023-07-13 16:02:07 -0500

    Vista de la app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { InicioComponent } from './components/inicio/inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    InicioComponent
   ],
   imports: [
     BrowserModule,
